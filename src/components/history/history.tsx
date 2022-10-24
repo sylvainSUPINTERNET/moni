60fc502af2afc61b71750c9ea550f055cedfac13
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2022-10-24 22:21:53 +0200

    e

--- src/components/history/history.tsx
+++ src/components/history/history.tsx
@@ -4,7 +4,6 @@ import { IHistoryService } from "../ws/Type";
 import {getInstanceWs, sendWsMessage, testWs} from "../ws/WsInstance";
 import * as signalR from "@microsoft/signalr"
 import { WS_URL_HISTORY_SIGNALR } from "../../config/ws.connections";
-
 const worker = new Worker("ws-workers.js")
 worker.postMessage("start woky")
 
@@ -16,71 +15,28 @@ interface IHistoryData {
     price: number;
 }
 
+// Avoid to create multiple listener / connection ( out of component and reredenring )
+let wsSubject = new Subject<IHistoryData>();
+const ws:any  = {
+    instance: new signalR.HubConnectionBuilder().withUrl(WS_URL_HISTORY_SIGNALR, {}).build(),
+    currentInstance: undefined
+}
+
+ws.instance.start().then(() => {
+    if ( !ws.currentInstance ) {
+        ws.currentInstance = ws.instance
+    }
+}).catch((e:any) => {
+    console.log("error", e)
+})
+
+ws.instance.on("NewHistoryEvent", (data:IHistoryData) => {  
+    wsSubject.next(data)
+});
+
+
 function HistoryBattle() {
-    console.log("History battle")
-    // https://www.digitalocean.com/community/tutorials/how-to-handle-async-data-loading-lazy-loading-and-code-splitting-with-react
-    // https://react-typescript-cheatsheet.netlify.app/docs/basic/getting-started/basic_type_example/
-    let [queueHistory, setQueueHistory] = useState<Array<any>>([]);
-
-    let [historyWsConn, setHistoryWsConn] = useState<signalR.HubConnection>();
-
     useEffect( () => {
-
-        let connection:signalR.HubConnection;
-        try {
-            connection = new signalR.HubConnectionBuilder()
-            // .configureLogging(signalR.LogLevel.Information)
-            .withUrl(WS_URL_HISTORY_SIGNALR, {})
-            .build();
-    
-            connection
-                .start()
-                .then( () => {
-                    setHistoryWsConn(connection);
-                })
-                .catch((err:any) => { console.log(err) } )
-        
-        } catch ( e ) {
-            console.log(e)
-        }
-
-
-        fetch("https://jsonplaceholder.typicode.com/comments")
-        .then( response => response.json() )
-        .then( data => {
-            let mock = [
-                {
-                    id: 1
-                },
-                {
-                    id: 2
-                },
-                {
-                    id: 3
-                },
-                {
-                    id: 4
-                },
-                {
-                    id: 5
-                },
-                {
-                    id: 6
-                }
-            ]
-            let arr = [...mock];
-            setQueueHistory(arr);
-        });
-
-        // cleanup
-        return () => {
-            try {
-                connection.stop()
-            } catch ( err ) {
-                console.log(err)
-            }
-        }
-
 
     }, [])
 
@@ -88,30 +44,34 @@ function HistoryBattle() {
       <div className="container mx-auto">
         <div className="mt-10">
             <p className="mt-10 mb-10">Hitory battle</p> 
-            {/* <HistoryQueue historyData={queueHistory}></HistoryQueue> */}
-            <HistoryQueueSignalR historyData={queueHistory} historyWsConn={historyWsConn}></HistoryQueueSignalR>
+            <HistoryQueueSignalR></HistoryQueueSignalR>
         </div>
       </div>
     )
   }
   
 
-function HistoryQueueSignalR ({historyData, historyWsConn}:{historyData:any, historyWsConn:signalR.HubConnection | undefined}):any {
-    let [arr, setArr] = useState<Array<IHistoryData>>([]);
-    
+function HistoryQueueSignalR ():any {
+    const [history, setHistory] = useState<IHistoryData[]>([]);
+
+    let sb = wsSubject.subscribe( (data:any) => {
+        console.log(data);
+        setHistory([...history, data]);
+    })
+
     useEffect( () => {
-        if ( historyWsConn ) {
-            historyWsConn.on("NewHistoryEvent", (historyData:IHistoryData) => {
-                console.log("NW HISTORY", historyData.winnerUserName)
-                setArr([...arr, historyData])
-            });
+        
+        // TODO call real API to get history current ( redis )
+
+        return () => { 
+            sb.unsubscribe();
         }
     })
 
     return ( 
         <div className="flex flex-wrap bg-gray-200 space-x-0.5">
          {
-            historyData.slice(0,5).map( (item: any, i:number) => {
+            history.slice(0,5).map( (item: any, i:number) => {
                 return <div className="flex-1">
                     <div id={`${i}`} className="text-white text-center bg-blue-600 py-2 border-2 border-sky-500">{item.id}</div>
                 </div>
